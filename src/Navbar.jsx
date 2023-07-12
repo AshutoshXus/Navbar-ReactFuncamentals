93b1dc8f4e831cf3df77d0a61c240aef259ce58c
AshutoshXus <34096718+AshutoshXus@users.noreply.github.com>
2023-07-12 16:57:52 +0530

    Add files via upload

---
+++ src/Navbar.jsx
@@ -0,0 +1,60 @@
+import logo from './logo.svg'
+import { links, social } from './data'
+import { FaBars } from 'react-icons/fa'
+import { useRef, useState } from 'react'
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false)
+  const linksContainerRef = useRef(null)
+  const linksRef = useRef(null)
+
+  const toggleLinks = () => {
+    setShowLinks(!showLinks)
+  }
+
+  const linkStyles = {
+    height: showLinks
+      ? `${linksRef.current.getBoundingClientRect().height}px`
+      : '0px',
+  }
+
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo}></img>
+          <button className="nav-toggle" onClick={toggleLinks}>
+            <FaBars></FaBars>
+          </button>
+        </div>
+        <div
+          className="links-container"
+          ref={linksContainerRef}
+          style={linkStyles}
+        >
+          <ul className="links" ref={linksRef}>
+            {links.map((link) => {
+              const { id, text, url } = link
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+
+        <ul className="social-icons">
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </nav>
+  )
+}
+export default Navbar
